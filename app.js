8c8d4d7789efa0460339cf5f81dbf93ef7a61bc4
PatricioCristaldo <patricio_cristaldo@outlook.com>
2023-05-17 17:31:50 -0300

    Se organiza en carpetas y se agrega estilo al main

---
+++ app.js
@@ -0,0 +1,16 @@
+/* Entry point */
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+/* Archivos estaticos */
+app.use(express.static(path.resolve(__dirname,'public')))
+
+/* Rutas */
+app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','index.html')))
+
+/* Levantamos el servidor con app listen */
+app.listen(port,function(){
+    return console.log(`Se levanta el servidor en http://localhost:${port}`)
+})
