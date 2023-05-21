7a52f676d4edef13e4de33dbb907a55f3b9afe1b
PatricioCristaldo <patricio_cristaldo@outlook.com>
2023-05-21 20:54:07 -0300

    Search page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,6 +1,26 @@
-import React from "react"
+import React, { useContext } from "react"
+import { CardPokemon } from '../components';
+import { useLocation } from "react-router-dom"
+import { PokemonContext } from '../context/PokemonContext';
+
 export const SearchPage = () => {
+  const location = useLocation()
+
+  const { globalPokemons } = useContext(PokemonContext);
+
+  const filteredPokemons = globalPokemons.filter(pokemon => pokemon.name.includes(location.state.toLowerCase()))
+  
   return (
-    <div>SearchPage</div>
+    <div className='container'>
+			<p className='p-search'>
+				Se encontraron <strong>{filteredPokemons.length}</strong>{' '}
+				resultados en tu búsqueda:
+			</p>
+			<div className='card-list-pokemon container'>
+				{filteredPokemons.map(pokemon => (
+					<CardPokemon pokemon={pokemon} key={pokemon.id} />
+				))}
+			</div>
+		</div>
   )
 }
